17d4360f80d5bf106640ecdec3daa3a071327ef8
John <john.groton@mymail.champlain.edu>
2023-09-25 12:05:45 -0400

    d

--- Week4/parse_info.bash
+++ Week4/parse_info.bash
@@ -7,3 +7,49 @@ then
 else
 	echo "File Does not exist"
 fi
+
+function menu() {
+
+	# clears the screen
+	clear
+
+	echo "[1] iptables"
+	echo "[2] cisco"
+	echo "[3] windows firewall"
+	echo "[4] Mac OS X"
+	read -p "Please enter a choice above: " choice
+
+	case "$choice" in
+
+		1) iptables
+		;;
+
+		2) cisco
+		;;
+
+		3) windowFirewall
+		;;
+
+		4) MacOsX
+		;;
+
+		*)
+
+			echo ""
+			echo "Invalid option"
+			echo ""
+			sleep 2
+
+			menu
+		;;
+
+	esac
+}
+
+function iptables()
+{
+	
+
+
+	esac
+}
