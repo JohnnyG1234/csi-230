fe58fcedc7a16493cde6a4b9290aa331e7d9152e
John <john.groton@mymail.champlain.edu>
2023-09-28 14:00:38 -0400

    ds

--- Week4/parse_info.bash
+++ Week4/parse_info.bash
@@ -2,7 +2,7 @@
 
 function create_badIPs(){
 
-	wget https://rules.emergingthreats.net/blockrules/emerging-drop.suricata.rules -0 /tmp/emerging-drop.suricata.rules
+	wget https://rules.emergingthreats.net/blockrules/emerging-drop.suricata.rules -O /tmp/emerging-drop.suricata.rules
 
 	egrep -o '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.0/[0-9]{1,2}' /tmp/emerging-drop.suricata.rules | sort -u | tee badips.txt
 }
