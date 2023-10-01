a7b2f3d2aa2b566bd7523f859d4db8cb55b6631d
John <john.groton@mymail.champlain.edu>
2023-10-01 14:50:35 -0400

    more test

--- Week5/Security_Scan.bash
+++ Week5/Security_Scan.bash
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+
+function checks()
+{
+    if [[ $2 != $3 ]]
+    then
+        echo "The $1 is not compliant. The current policy is: $2"
+    else
+        echo "The $1 is compliant. Current value $3"
+    fi
+}
+
+
 pmax=$(egrep -i '^PASS_MAX_DAYS' /etc/login.defs | awk ' { print $2 } ')
 
-if [[ "${pmax}" != "120" ]]
-then
-    echo "The password max days policy is not compliant. The current policy is: ${pmax}"
-else
-    echo "The password max days policy is compliant"
-fi
+# Check for password max
+checks "Password Max Days" "365" "${pmax}"
