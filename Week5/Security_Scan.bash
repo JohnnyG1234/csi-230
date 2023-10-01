9def1268d7dce41df6fa0eec2981a0b6e4fa923d
John <john.groton@mymail.champlain.edu>
2023-10-01 17:01:17 -0400

    This will work

--- Week5/Security_Scan.bash
+++ Week5/Security_Scan.bash
@@ -46,5 +46,5 @@ checks "IP Forwading" "0" "${chkIpForward: -1}"
 chkICMPRe=$(sysctl net.ipv4.conf.all.send_redirects | awk ' {print $3 } ')
 checks "ICMP redirects" "0" "${chkICMPRe}"
 
-chkUid=$(stat /etc/crontab)
+chkUid=$(stat /etc/crontab | awk ' {print $21 } ')
 checks "Uid in /etc/crontab" "0/" "${chkUid}"
