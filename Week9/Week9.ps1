14bd71d402a8dbf7ce8a4303208c11ee97a0c8fc
JohnnyG1234 <89805746+JohnnyG1234@users.noreply.github.com>
2023-10-26 14:55:18 -0400

    Adding week 9 stuff

---
+++ Week9/Week9.ps1
@@ -0,0 +1,18 @@
+﻿# Getting processes
+Get-Process | Select-Object PorcessName, Path, ID | `
+Export-Csv -Path "C:\Users\jgrot\OneDrive\Desktop\CSI-230\csi-230\Week9\myProcesses.csv"
+
+# Getting Services
+Get-Service | Export-Csv -Path "C:\Users\jgrot\OneDrive\Desktop\CSI-230\csi-230\Week9\myServices.csv"
+
+# Getting DNS and DHCP server info
+Get-WmiObject -Class Win32_NetworkAdapterConfiguration | select DNSDomain, DHCPServer, DefaultIPGateway, IPAddress
+
+# Starting windows calculator
+Start-Process Calc.exe
+
+Start-Sleep -Seconds 3
+
+Stop-Process -name "CalculatorApp"
+
+
