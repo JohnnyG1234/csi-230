f249fcda005752ca203c582e53e4c2f9e1c1a867
John <john.groton@mymail.champlain.edu>
2023-11-02 14:24:29 -0400

    adding week 10 stuff

---
+++ week10/week10GetServiceLogs.ps1
@@ -0,0 +1,71 @@
+﻿# lists all registered services
+# promts the user if they want to view all, stopped or runing services
+
+function prompt_user(){
+    cls
+
+    # promt the user for what they want to see
+    $userChoice = Read-Host -Prompt "Do you want to see all, running, or stopped services? (press q to quit)"
+
+    # If user wants to quit then... THEN QUIT!!!! What else would you do? Sorry that was rude my bad
+    if ($userChoice -match "^[qQ]$"){
+        break
+    }
+
+    # Determine what function to run next based on user input
+    if ($userChoice -match "^all$"){
+        get_all_services
+    }
+    elseif ($userChoice -match "^stopped$"){
+        get_stopped_services
+    }
+    elseif ($userChoice -match "^running$"){
+        get_running_services
+    }
+    else{
+        Write-Host "Invalid input, try again"
+        sleep 2
+        prompt_user
+    }
+
+}
+
+
+function get_all_services(){
+    cls
+    
+    Get-Service
+
+    # puase the screen until the user is ready to procced
+    Read-Host -Prompt "Press enter when you are done"
+
+    prompt_user
+}
+
+
+function get_stopped_services(){
+    cls
+    
+    Get-Service | where { $_.Status -eq "Stopped" }
+
+    # puase the screen until the user is ready to procced
+    Read-Host -Prompt "Press enter when you are done"
+
+    prompt_user
+}
+
+
+function get_running_services(){
+    cls
+    
+    Get-Service | where { $_.Status -eq "Running" }
+
+    # puase the screen until the user is ready to procced
+    Read-Host -Prompt "Press enter when you are done"
+
+    prompt_user
+}
+
+
+
+prompt_user
---
+++ week10/week10VidCode.ps1
@@ -0,0 +1,74 @@
+﻿# Storyline: View the event logs, check for valid log, and print the results
+
+
+function select_log(){
+
+    cls
+    #List all event logs
+    $theLogs = Get-EventLog -list | select log
+    $theLogs | Out-Host
+
+    # initialize the array to stor the logs
+    $arrLog = @()
+
+    foreach ($tempLog in $theLogs){
+        # add each log to the array
+        $arrLog += $tempLog
+    }
+
+    # promt the user for the log to view, or to quit the program
+    $readLog = Read-Host -Prompt "Please enter a log from the list above or 'q' to quit the program"
+    
+    # check if the user want to quit
+    if ($readLog -match  "^[qQ]$"){
+        break
+    }
+
+    log_check -logToSearch $readLog
+}
+
+function log_check(){
+    
+    Param([string]$logToSearch)
+
+    # format the user input
+    # example @{Log=security}
+    $theLog = "^@{Log=" + $logToSearch + "}$"
+
+    # search the array for the exact hashtable string
+    if ($arrLog -match $theLog){
+        Write-Host -BackgroundColor Green -ForegroundColor white "Please wait a few moment to retrieve the log entries"
+        sleep 2
+
+        # call the function to view the log
+        view_log -logToSearch $logToSearch
+    } else {
+        Write-Host -BackgroundColor Red -ForegroundColor White "The log specified does not exist"
+        
+        sleep 2
+        select_log
+    }
+}
+
+
+
+
+function view_log(){
+
+    cls
+
+    # get the logs
+    Get-EventLog -Log $logToSearch -Newest 10
+
+    # puase the screen until the user is ready to procced
+    Read-Host -Prompt "Press enter when you are done"
+
+    select_log
+
+}
+
+
+# Run the select log function
+select_log
+
+
