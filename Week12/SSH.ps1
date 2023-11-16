eb44d532887504c4e53c1c88463d3f1b517adb06
John <john.groton@mymail.champlain.edu>
2023-11-16 13:43:43 -0500

    adding week 12 code

---
+++ Week12/SSH.ps1
@@ -0,0 +1,16 @@
+﻿# Login to a remote SSH server
+#New-SSHSession -ComputerName '192.168.4.22' -Credential (Get-Credential sys320)
+
+while ($True) {
+
+    # Add a promt to run commands
+    $the_cmd = Read-Host -Prompt "Please enter a command"
+
+    # Run command on remote SSH server
+    (Invoke-SSHCommand -index 0 $the_cmd).Output
+
+}
+
+
+Set-SCPFile -ComputerName '192.168.4.22' -Credential (Get-Credential sys320) `
+-RemotePath '/home/sys320' -LocalFile '.\tedx.jpeg'
